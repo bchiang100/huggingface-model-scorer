b33b9339a9c27c42116ca88c5b2f45d76fc455bd
Evan Zhang <evanzh128@gmail.com>
2025-09-21 19:51:27 -0400

    added an (untested) dataset quality measuring metrics, slight tweaks to the base code and more

--- src/metrics/metrics_base.py
+++ src/metrics/metrics_base.py
@@ -1,15 +1,22 @@
 from abc import ABC, abstractmethod
-
 from parsing.url_base import *
 import urllib.parse
 
 
 class Metric(ABC):
+    '''
+    Metrics can expect that it will only run on Site objects (Model, Dataset, Codebase)
+
+    Each metric must implement the abstract method calculate()  
+    
+    '''
     def __init__(self, asset):
 
         self.asset: Site = asset
         self.latency: float = 0.0
         self.score: float = 0.0
+        self.owner: str = ""
+        self.asset_id: str = ""
 
     @property
     def url(self):
@@ -27,6 +34,8 @@ class Metric(ABC):
                 path_parts = parsed_url.path.strip('/').split('/')
                 if len(path_parts) >= 2:
                     owner, model = path_parts[0], path_parts[1]
+                    self.owner = owner
+                    self.asset_id = model
                     return f"https://huggingface.co/api/models/{owner}/{model}"
                 else:
                     raise ValueError("Invalid Hugging Face Model URL format.")
@@ -36,6 +45,8 @@ class Metric(ABC):
                 path_parts = parsed_url.path.strip('/').split('/')
                 if len(path_parts) >= 2:
                     owner, dataset = path_parts[0], path_parts[1]
+                    self.owner = owner
+                    self.asset_id = dataset
                     return f"https://huggingface.co/api/datasets/{owner}/{dataset}"
                 else:
                     raise ValueError("Invalid Hugging Face Dataset URL format.")
@@ -45,6 +56,8 @@ class Metric(ABC):
                 path_parts = parsed_url.path.strip('/').split('/')
                 if len(path_parts) >= 2:
                     owner, repo = path_parts[0], path_parts[1]
+                    self.owner = owner
+                    self.asset_id = repo
                     return f"https://api.github.com/repos/{owner}/{repo}"
                 else:
                     raise ValueError("Invalid GitHub URL format.")
