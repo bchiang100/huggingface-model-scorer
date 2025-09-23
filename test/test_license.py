a9344c9fcd401f232c6a7a0607dba0c68d1ff3d1
danielklwu <danielklwu@gmail.com>
2025-09-23 18:00:54 -0400

    modified to prioritize README LLM parsing

--- test/test_license.py
+++ test/test_license.py
@@ -4,17 +4,34 @@ from src.parsing.url_base import *
 
 if __name__ == "__main__":
     # Example HuggingFace model URLs
-    example_url = [
-        "https://huggingface.co/google/gemma-3-270m/tree/main",
-        "https://huggingface.co/timm/mobilenetv3_small_100.lamb_in1k",
-        "https://huggingface.co/tencent/SRPO"
+    print("========== High License Score Tests ==========")
+    high_examples = [
+        "https://huggingface.co/a32fsdf23/mytest12345", # LGPL 2.1
+        "https://huggingface.co/timm/mobilenetv3_small_100.lamb_in1k", # apache 2.0
+        "https://huggingface.co/FacebookAI/roberta-large", # mit
     ]
 
-    for i, url in enumerate(example_url):
-        m = Model(example_url[i])
+    for i, url in enumerate(high_examples):
+        m = Model(high_examples[i])
         license = License(m)
-        print(f"Extracted license #{i+1}: {license._getLicense()}")
+        # print(f"Extracted license #{i+1}: {license._getLicense()}")
         print(f"Calculated score #{i+1}: {license.calculate()}")
         # print(f"Stored score #{i+1}: {license.score}")
         print(f"Calculated latency #{i+1}: {license.latency}")
         print()
+
+    print("========== Low License Score Tests ==========")
+    low_examples = [
+        "https://huggingface.co/tencent/SRPO", #tencent-hunyuan
+        "https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment-latest", # cc-by-4.0
+        "https://huggingface.co/driaforall/mem-agent" # not mentioned
+    ]
+
+    for i, url in enumerate(low_examples):
+        m = Model(low_examples[i])
+        license = License(m)
+        # print(f"Extracted license #{i+1}: {license._getLicense()}")
+        print(f"Calculated score #{i+1}: {license.calculate()}")
+        # print(f"Stored score #{i+1}: {license.score}")
+        print(f"Calculated latency #{i+1}: {license.latency}")
+        print()    
